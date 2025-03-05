a7ebb2013c79309f0c97e1cff7160fe51f9d96a5
Michael Lourenco <michael.lourenco@pixter.com.br>
2025-03-05 15:40:38 -0300

    add cookie consent to app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Providers } from "./providers";
 import { AuthProvider } from "./auth-provider";
 import { Toaster } from "sonner";
+import CookieConsent from "@/components/CookieConsent";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
         <AuthProvider>
           <Providers>
             {children}
+            <CookieConsent />
             <Toaster
               richColors
               position="top-right"
